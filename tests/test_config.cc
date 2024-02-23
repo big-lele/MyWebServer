c627d678319d9ea7986ea0155f916791aa8df40c
big-lele <zml201830330255@163.com>
2024-02-23 14:28:05 +0800

    Support vector Contains Partial Specialization

--- tests/test_config.cc
+++ tests/test_config.cc
@@ -2,11 +2,15 @@
 #include "webserver/log.h"
 #include <yaml-cpp/yaml.h>
 
+//约定优于配置
 webserver::ConfigVar<int>::ptr g_int_value_config =
         webserver::Config::Lookup("system.port", (int)8080, "system port");
 
 webserver::ConfigVar<float>::ptr g_float_value_config =
         webserver::Config::Lookup("system.value", (float)10.2f, "system value");
+
+webserver::ConfigVar<std::vector<int> >::ptr g_int_vec_value_config =
+        webserver::Config::Lookup("system.int_vec", std::vector<int>{1, 2}, "system int vec");
 
 void print_yaml(const YAML::Node& node, int level){
         if(node.IsScalar()){
@@ -44,12 +48,21 @@ void test_yaml(){
 void test_config(){
         WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "before: " << g_int_value_config->getValue();
         WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "before: " << g_float_value_config->toString();
+        auto v = g_int_vec_value_config->getValue();
+	for(auto& i : v){
+	        WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "before int_vec: " << i;
+	}
 
 	YAML::Node root = YAML::LoadFile("/root/webServer/MyWebServer/bin/conf/log.yml");
 	webserver::Config::LoadFromYaml(root);
 
 	WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "after: " << g_int_value_config->getValue();
         WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "after: " << g_float_value_config->toString();
+
+	v = g_int_vec_value_config->getValue();
+        for(auto& i : v){
+                WEBSERVER_LOG_INFO(WEBSERVER_LOG_ROOT()) << "after int_vec: " << i;
+        }
 }
 
 int main(int argc, char** argv){
--- webserver/config.h
+++ webserver/config.h
@@ -30,8 +30,51 @@ protected:
 	std::string m_description;
 };
 
-//定义实现类
+//F from_type, T to_type
+template<class F, class T>
+class LexicalCast{
+public:
+	T operator()(const F& v){
+	        return boost::lexical_cast<T>(v);
+	}
+};
+
+//针对vector进行了片特化
 template<class T>
+class LexicalCast<std::string, std::vector<T> >{
+public:
+        std::vector<T> operator()(const std::string& v){
+	        YAML::Node node = YAML::Load(v);
+		typename std::vector<T> vec;
+		std::stringstream ss;
+		for(size_t i = 0; i < node.size(); ++i){
+		        ss.str("");
+			ss << node[i];
+			vec.push_back(LexicalCast<std::string, T>()(ss.str()));
+		}
+		return vec;
+	}
+};
+
+template<class T>
+class LexicalCast<std::vector<T>, std::string>{
+public:
+        std::string operator()(const std::vector<T>& v){
+                YAML::Node node;
+                for(auto& i: v){
+		        node.push_back(YAML::Load(LexicalCast<T, std::string>()(i)));
+		}
+		std::stringstream ss;
+		ss << node;
+                return ss.str();
+        }
+};
+
+//定义实现类
+//FromStr T operator() (const std::string&)
+//ToStr std::string operator() (const T&)  //实现复杂的类型转化
+template<class T, class FromStr = LexicalCast<std::string, T>
+                , class ToStr = LexicalCast<T, std::string> > // 支持序列化和反序列化
 class ConfigVar : public ConfigVarBase{
 public:
 	typedef std::shared_ptr<ConfigVar> ptr;
@@ -45,7 +88,8 @@ public:
 
 	std::string toString() override{
 	        try{
-		        return boost::lexical_cast<std::string>(m_val); //类型转换
+		        //return boost::lexical_cast<std::string>(m_val); //只能支持简单的类型转换
+			return ToStr()(m_val);
 		} catch(std::exception& e){
 		        WEBSERVER_LOG_ERROR(WEBSERVER_LOG_ROOT()) << "ConfigVar::toString exception"
 				<< e.what() << "convert:" << typeid(m_val).name() << "to string";
@@ -56,7 +100,8 @@ public:
 
 	bool fromString(const std::string& val) override{
 	       try{
-		       m_val = boost::lexical_cast<T>(val);
+		       //m_val = boost::lexical_cast<T>(val);
+		       setValue(FromStr()(val));
 	       } catch(std::exception& e){
 	               WEBSERVER_LOG_ERROR(WEBSERVER_LOG_ROOT()) << "ConfigVar::toString exception"
                                 << e.what() << "convert: string to " << typeid(m_val).name();
